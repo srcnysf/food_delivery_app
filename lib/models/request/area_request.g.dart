fbb669d7406a0fe34964f6537f14fd9526b05944
Sercan Yusuf <sercan.yusuf@leanscale.com>
2021-03-22 01:07:27 +0300

    refactor: model classes

---
+++ lib/models/request/area_request.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'area_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AreaRequest _$AreaRequestFromJson(Map<String, dynamic> json) {
+  return AreaRequest(
+    a: json['a'] as String?,
+  );
+}
+
+Map<String, dynamic> _$AreaRequestToJson(AreaRequest instance) =>
+    <String, dynamic>{
+      'a': instance.a,
+    };
---
+++ lib/models/request/search_request.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'search_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SearchRequest _$SearchRequestFromJson(Map<String, dynamic> json) {
+  return SearchRequest(
+    s: json['s'] as String?,
+  );
+}
+
+Map<String, dynamic> _$SearchRequestToJson(SearchRequest instance) =>
+    <String, dynamic>{
+      's': instance.s,
+    };
